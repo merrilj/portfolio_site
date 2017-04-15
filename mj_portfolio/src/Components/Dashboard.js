d2cf5fb3b6db7a70e774232b5af9c60be03f63bf
Merril Jeffs <merril@Merrils-MBP.lan>
2017-04-15 14:46:53 -0600

    basic site setup

--- mj_portfolio/src/Components/Dashboard.js
+++ mj_portfolio/src/Components/Dashboard.js
@@ -4,8 +4,18 @@ export default class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>Merril Jeffs</h1>
+        <h1 style={styles.content}>Merril Jeffs</h1>
+        <h2 style={styles.content}>Coming Soon!</h2>
       </div>
-    );
+    )
+  }
+}
+
+const styles = {
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    fontSize: '3em',
+    paddingTop: '2em'
   }
 }
