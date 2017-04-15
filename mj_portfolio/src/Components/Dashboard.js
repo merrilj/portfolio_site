e627a289e41ce1b7d1d00f60b6469daeabf51cde
Merril Jeffs <merril@Merrils-MBP.lan>
2017-04-15 15:49:10 -0600

    basic styling

--- mj_portfolio/src/Components/Dashboard.js
+++ mj_portfolio/src/Components/Dashboard.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { SlideUp } from 'animate-components'
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1 style={styles.content}>Merril Jeffs</h1>
-        <h2 style={styles.content}>Coming Soon!</h2>
+      <div style={styles.content}>
+        <SlideUp>
+        <h1 style={styles.header}>Merril Jeffs Portfolio</h1>
+        <h2 style={styles.tagline}>Coming Soon!</h2>
+        </SlideUp>
       </div>
     )
   }
@@ -15,7 +18,19 @@ const styles = {
   content: {
     display: 'flex',
     justifyContent: 'center',
+  },
+  header: {
     fontSize: '3em',
-    paddingTop: '2em'
+    paddingTop: '2em',
+    color: '#87cddd',
+    fontFamily: 'Work Sans, sans-serif',
+    fontSize: '4em',
+    textAlign: 'center'
+  },
+  tagline: {
+    color: '#87cddd',
+    fontFamily: 'Work Sans, sans-serif',
+    fontSize: '3em',
+    textAlign: 'center'
   }
 }
