d070a7a383fa4692f9fd2bcde6dcfb961bf752a1
Merril Jeffs <merril@Merrils-MBP.lan>
2017-05-03 17:23:45 -0600

    added card component for projects

--- mj_portfolio/src/Components/Dashboard.js
+++ mj_portfolio/src/Components/Dashboard.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
 import { SlideUp } from 'animate-components'
+import { Card, Icon } from 'semantic-ui-react'
+
+const extra = (
+  <a>
+    <Icon name='user' />
+    16 Friends
+  </a>
+)
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div style={styles.content}>
         <SlideUp>
-        <h1 style={styles.header}>Merril Jeffs Portfolio</h1>
-        <h2 style={styles.tagline}>Coming Soon!</h2>
+          <h1 style={styles.header}>Merril Jeffs</h1>
+          <h2 style={styles.tagline}>Software Developer</h2>
+          <div>
+            <Card
+              image='/assets/images/avatar/large/elliot.jpg'
+              header='Elliot Baker'
+              meta='Friend'
+              description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+              extra={extra}
+            />
+            <Card
+              image='/assets/images/avatar/large/elliot.jpg'
+              header='Elliot Baker'
+              meta='Friend'
+              description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+              extra={extra}
+            />
+          </div>
+
         </SlideUp>
       </div>
     )
@@ -21,16 +46,18 @@ const styles = {
   },
   header: {
     fontSize: '3em',
-    paddingTop: '2em',
+    paddingTop: '1em',
     color: '#87cddd',
     fontFamily: 'Work Sans, sans-serif',
     fontSize: '4em',
-    textAlign: 'center'
+    textAlign: 'center',
+    marginBottom: '0'
   },
   tagline: {
     color: '#87cddd',
     fontFamily: 'Work Sans, sans-serif',
-    fontSize: '3em',
-    textAlign: 'center'
+    fontSize: '2.5em',
+    textAlign: 'center',
+    marginTop: '0'
   }
 }
