cf09f51dcc8a145f97d8fc28c143a0f61f07d381
Merril Jeffs <merril@Merrils-MacBook-Pro.local>
2017-05-08 15:33:09 -0600

    trying to add social icons

--- mj_portfolio/src/Components/Dashboard.js
+++ mj_portfolio/src/Components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { SlideUp, FadeInDown } from 'animate-components'
-import { Button } from 'semantic-ui-react'
+import { Icon } from 'semantic-ui-react'
 
 export default class Dashboard extends Component {
   render() {
