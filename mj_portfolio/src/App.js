dad451613bd9d40f2ec87039d8a272c574a41cb3
Merril Jeffs <merril@Merrils-MBP.lan>
2017-05-11 15:44:29 -0600

    removed main and matched resume color

--- mj_portfolio/src/App.js
+++ mj_portfolio/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
 import Dashboard from './Components/Dashboard'
-import Main from './Components/Main'
 
 class App extends Component {
   render() {
