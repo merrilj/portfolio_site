fa321b84d6f44bf4fb24182b8cbac3277e09e01b
Merril Jeffs <merril@Merrils-MBP.lan>
2017-05-15 17:08:36 -0600

    finishing about component

--- mj_portfolio/src/Components/About.js
+++ mj_portfolio/src/Components/About.js
@@ -5,8 +5,12 @@ export default class About extends Component {
     return (
       <div>
         <div style={styles.merril}>
+          <img src="../Images/merril.JPG" style={styles.image} />
           <h1 style={styles.header}>Merril Jeffs</h1>
-          <h2 style={styles.tagline}>Software Developer</h2>
+          <h2 style={styles.title}>Software Developer</h2>
+          <p style={styles.summary}>
+            I am a Full Stack Developer who thinks its great to build cool stuff. Beyond coding, I have a passion for aviation and travel, and I plan to get my private pilots license soon. I also love playing sports, cheering for the Broncos, and a little hiking and camping in between.
+          </p>
         </div>
       </div>
     )
@@ -15,7 +19,7 @@ export default class About extends Component {
 
 const styles = {
   merril: {
-    
+
   },
   header: {
     fontSize: '3em',
@@ -25,11 +29,23 @@ const styles = {
     fontSize: '4em',
     textAlign: 'center',
   },
-  tagline: {
+  title: {
     color: '#fff',
     fontFamily: 'Work Sans, sans-serif',
     fontSize: '2.5em',
     textAlign: 'center',
     marginTop: '0'
   },
+  summary: {
+    fontFamily: 'Work Sans, sans-serif',
+    color: '#fff',
+    fontSize: '1.5em',
+    width: '50%',
+    textAlign: 'center'
+  },
+  image: {
+    width: '200px',
+    height: '400px',
+
+  }
 }
