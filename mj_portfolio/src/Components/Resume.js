216b791728d9f4866f2af8f5b300923a506ee2e7
Merril Jeffs <merril@Merrils-MBP.lan>
2017-05-15 10:30:19 -0600

    added resume component

--- mj_portfolio/src/Components/Resume.js
+++ mj_portfolio/src/Components/Resume.js
@@ -4,8 +4,8 @@ export default class Resume extends Component {
   render () {
     return (
       <div>
-        <object data="test.pdf" type="application/pdf" width="300" height="200">
-          alt : <a href="test.pdf">test.pdf</a>
+        <object data="../Images/Resume.pdf" type="application/pdf" width="600" height="1200">
+          alt : <a href="../Images/Resume.pdf">test.pdf</a>
         </object>
       </div>
     )
